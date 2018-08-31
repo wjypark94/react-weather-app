43ffcf9d85a28f49cf7245fecc84b3492ccb0caf
William Park <wjypark94@gmail.com>
2018-08-31 16:52:05 -0700

    able to render basic weather temperature

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items:[]
+    };
+  }
+
   componentDidMount(){
     fetch("https://api.openweathermap.org/data/2.5/forecast?q=los angeles&mode=json&APPID=b034d607850d9e3b5543e5449c3e7364")
     .then(res => res.json())
-    .then(
-      (result) =>{
-        console.log(result)
-      }
-    )
+    .then(items => this.setState({ items: items.list}))
   }
   render() {
+    const { items } = this.state;
+    console.log(items);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Weather App</h1>
-        </header>
-        <p className="App-intro">
-          Add all the weather data here
-        </p>
+      <div>
+        <h1> Weather Report in Fahrenheit</h1>
+      <ul>
+        {items.map(item => (
+          <li key={item.dt}>
+            {((item.main.temp-273)*1.8)+32}
+          </li>
+        ))}
+      </ul>
       </div>
     );
   }
