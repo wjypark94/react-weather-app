8ac695c011d617c0a2086b518cc7113abe20ee0c
William Park <wjypark94@gmail.com>
2018-08-31 18:08:02 -0700

    fetch in search component

--- src/App.js
+++ src/App.js
@@ -3,32 +3,11 @@ import './App.css';
 import Search from './components/Search';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      items:[]
-    };
-  }
-
-  componentDidMount(){
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=los angeles&mode=json&APPID=b034d607850d9e3b5543e5449c3e7364")
-    .then(res => res.json())
-    .then(items => this.setState({ items: items.list}))
-  }
   render() {
-    const { items } = this.state;
-    console.log(items);
     return (
       <div>
         <h1> Weather Report in Fahrenheit</h1>
         <Search/>
-      <ul>
-        {items.map(item => (
-          <li key={item.dt}>
-            {((item.main.temp-273)*1.8)+32}
-          </li>
-        ))}
-      </ul>
       </div>
     );
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,20 @@
 import React, {Component} from 'react';
 
 class Search extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            items:[]
+        };
+    }
+
+    componentDidMount(){
+        fetch("https://api.openweathermap.org/data/2.5/forecast?q=los angeles&mode=json&APPID=b034d607850d9e3b5543e5449c3e7364")
+        .then(res => res.json())
+        .then(items => this.setState({ items: items.list}))
+    }
     render(){
+        const { items } = this.state;
         return(
             <div>
                 <h2>Check the weather here!</h2>
@@ -9,6 +22,13 @@ class Search extends Component {
                     <input type="text" placeholder="Enter a city.."></input>
                     <button type="submit">Search</button>
                 </form>
+                <ul>
+                    {items.map(item => (
+                    <li key={item.dt}>
+                    {((item.main.temp-273)*1.8)+32}
+                    </li>
+                    ))}
+                    </ul>
             </div>
         )
     }
